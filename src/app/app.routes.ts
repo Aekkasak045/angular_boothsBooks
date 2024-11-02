a3c4fe2ad77223f95fe232ce891c3c76d1dffd2e
Aekkasak Makhamjan <eqadzc@gmail.com>
2024-11-02 23:13:44 +0700

    update_butstillbug

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { MainComponent } from './page/main/main.component';
 import { BoothpageComponent } from './page/boothpage/boothpage.component';
 import { LoginComponent } from './page/login/login.component';
 import { RegisterComponent } from './page/register/register.component';
+import { ViewprofileComponent } from './page/viewprofile/viewprofile.component';
+import { BookingComponent } from './page/booking/booking.component';
 
 export const routes: Routes = [
     {path:'',component: FirstpageComponent},
@@ -11,5 +13,7 @@ export const routes: Routes = [
     {path:'booths',component: BoothpageComponent},
     {path:'login',component: LoginComponent},
     {path:'register',component: RegisterComponent},
+    {path:'viewprofile',component:ViewprofileComponent},
+    {path:'booking',component:BookingComponent}
     
 ];
--- src/app/page/booking/booking.component.ts
+++ src/app/page/booking/booking.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit} from '@angular/core';
+import { HttpClient,HttpClientModule } from '@angular/common/http';
+import { DataService } from '../../service/data.service';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../../component/header/header.component';
 @Component({
   selector: 'app-booking',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,HttpClientModule,HeaderComponent],
   templateUrl: './booking.component.html',
   styleUrl: './booking.component.scss'
 })
 export class BookingComponent {
+  bookings: any[] = [];
 
+  constructor(private dataService: DataService,private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadBookings();
+  }
+
+  loadBookings(): void {
+    this.http.get<any[]>(this.dataService.apiEndpoint+'/get_bookings').subscribe(
+      (data) => {
+        this.bookings = data;
+      },
+      (error) => {
+        console.error('Error fetching bookings:', error);
+      }
+    );
+  }
 }
